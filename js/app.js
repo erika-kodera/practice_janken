4d87f8907fa8cd65eadaa0dda5e3eb684ae4d461
erika-kodera <98879043+erika-kodera@users.noreply.github.com>
2022-07-05 12:05:15 +0900

    コンピューターの手を決める
    コンピューターのジャンケンの手を決めれるように記載した。

--- js/app.js
+++ js/app.js
@@ -12,5 +12,21 @@ if (hum !== GU && hum !== CHOKI && hum !== PA) {
   //入力値が不適切な場合
   alert('入力値をうまく認識できませんでした。ブラウザを再読み込みすると、もう一度挑戦できます。');
 } else {
-  
+
+  //コンピューターの手を決める
+  let com = Math.floor(Math.random() * 3) + 1;
+
+  //コンピューターの手の名前
+  let comHandName = '';
+  switch (com) {
+    case GU:
+      comHandName = 'グー';
+      break;
+    case CHOKI:
+      comHandName = 'チョキ';
+      break;
+    case PA:
+      comHandName = 'パー';
+      break;
+  }
 }
